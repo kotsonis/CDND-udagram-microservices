0d3de143ff74d66660ea1e764f0b997578e4cb27
kotsonis <stefankotsonis@gmail.com>
2021-03-22 00:00:10 +0100

    fix: CORS error
    update Access-Control-Allow-Origin to '*' instead of 'localhost:8100'.

--- udagram-api-feed/src/server.ts
+++ udagram-api-feed/src/server.ts
@@ -21,7 +21,7 @@ const c = config.dev;
   app.use(bodyParser.json());
   //CORS Should be restricted
   app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "http://localhost:8100");
+    res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
     next();
   });
